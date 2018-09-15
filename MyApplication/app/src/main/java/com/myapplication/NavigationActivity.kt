70a8d2be45a7412c3472a0970aae96f58f04df26
ozmank <khalid.usman7@gmail.com>
2018-09-15 23:11:10 +0400

    adding navigation

--- MyApplication/app/src/main/java/com/myapplication/NavigationActivity.kt
+++ MyApplication/app/src/main/java/com/myapplication/NavigationActivity.kt
@@ -1,7 +1,21 @@
 package com.myapplication
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_navigation.*
 
 class NavigationActivity: AppCompatActivity(){
+
+
+    private lateinit var navigationView: BottomNavigationView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+        navigationView = navigation_view
+    }
 
 }
